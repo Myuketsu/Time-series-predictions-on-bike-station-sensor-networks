54436e5cb0c5b805e2922bc430d2c7399d027ce9
Mine <myuketsu@gmail.com>
2024-02-25 18:24:47 +0100

    tmp

--- view/map.py
+++ view/map.py
@@ -1,36 +1,75 @@
 import dash_leaflet as dl
 from data.city.load_cities import City
 
-ICONS = {
-    'Red': {
-        'iconUrl': 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-        'shadowUrl': 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+ICONS_MAKRERS = {
+    'red': {
+        'iconUrl': './assets/pictures/marker/marker-icon-red.png',
+        'shadowUrl': './assets/pictures/marker/marker-icon-shadow.png',
         'iconSize': [25, 41],
         'iconAnchor': [12, 41],
         'popupAnchor': [1, -34],
         'shadowSize': [41, 41]
     },
-    'Blue': None
+    'blue': None
 }
 
-def get_markers(city: City, default_markers_colors: str='Blue') -> list[dl.Marker]:
+ICONS_DIV_MAKRERS = {
+    'red': dict(
+        className='div_marker_map_red',
+        iconSize=[17, 17]
+    ),
+    'blue': dict(
+        className='div_marker_map_blue',
+        iconSize=[17, 17]
+    ),
+}
+
+def get_markers(city: City, distribution: dict[int, str]=[]) -> list[dl.Marker]:
     return [
         dl.Marker(
+            position=[row['latitude'], row['longitude']],
+            children=[dl.Tooltip(row['code_name'])],
+            id={'type': 'marker', 'code_name': row['code_name'], 'index': index},
+            icon=ICONS_MAKRERS[distribution.get(index, 'blue')],
+            n_clicks=0
+        ) for index, row in city.df_coordinates.iterrows()
+    ]
+
+def get_div_markers(city: City, default_markers_colors: str='blue') -> list[dl.Marker]:
+    return [
+        dl.DivMarker(
             position=[row['latitude'], row['longitude']],
             children=[
                 dl.Tooltip(row['code_name']), 
             ],
             id={'type': 'marker', 'code_name': row['code_name'], 'index': index},
             n_clicks=0,
-            icon=ICONS[default_markers_colors]
+            iconOptions=ICONS_DIV_MAKRERS[default_markers_colors]
         ) for index, row in city.df_coordinates.iterrows()
     ]
 
-def viewport_map(city: City, id: str, default_markers_colors: str='Blue'):
+def get_map_children(city: City, distribution: list[list[dict]]=None, default_markers_colors: str='blue'):
+    children = [dl.TileLayer()]
+    for index, row in city.df_coordinates.iterrows():
+        children.append(
+            dl.Marker(
+                position=[row['latitude'], row['longitude']],
+                children=[dl.Tooltip(row['code_name'])],
+                id={'type': 'marker', 'code_name': row['code_name'], 'index': index},
+                icon=ICONS_MAKRERS[default_markers_colors],
+                n_clicks=0
+            )
+        )
+
+def viewport_map(city: City, id: str, use_img_icon: bool=True, default_markers_colors: str='blue'):
+    if use_img_icon:
+        markers = get_markers(city, default_markers_colors)
+    else:
+        markers = get_div_markers(city, default_markers_colors)
     return dl.Map(
         [
             dl.TileLayer(),  # OpenStreetMap par défaut
-        ] + get_markers(city, default_markers_colors),
+        ] + markers,
         center=city.centroid,
         bounds=city.bounds,
         maxBounds=city.bounds,
